e006b2fe732b5785ced72cbe18ee00829cf1fd8b
Alexandr <102781985+Eshenok@users.noreply.github.com>
2023-01-26 21:01:19 +0600

    fix 404 page

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,6 +2,7 @@ const options = {
   origin: [
     'http://localhost:3000',
     'https://movies-explorer.eshenok.nomoredomains.club',
+    'http://movies-explorer.eshenok.nomoredomains.club',
   ],
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
   preflightContinue: false,
